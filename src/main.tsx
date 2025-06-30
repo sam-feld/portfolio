3a3ed8ce0b66babb6ec26de518698437f0e8f938
sam <shmulifeld@Shmulis-MacBook-Air.local>
2025-06-30 18:24:25 -0400

    changed to hashrouter

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import MainLayout from './components/MainLayout.tsx'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import ScrollToTop from './utils/ScrollToTop.tsx'
 import Home from './pages/Home.tsx'
 import Project from './pages/Project.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ScrollToTop />
       <Routes>
         <Route path="/" element={<MainLayout />}>
@@ -17,6 +17,6 @@ createRoot(document.getElementById('root')!).render(
           <Route path="/project/:id" element={<Project/>}></Route>
         </Route> 
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
